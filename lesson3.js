4b0cb3d6e48f1dc0a9fbae4a4a1bb1776805998b
Nhan Quốc Việt <qviet092@gmail.com>
2024-10-16 23:49:38 +0700

    first commit

---
+++ lesson3.js
@@ -0,0 +1,6 @@
+var message = "Hello world";
+console.log(message);
+var sum = function (a, b) {
+    return a + b;
+};
+console.log('>>> sum = ', sum(2, 2));
---
+++ lesson3.ts
@@ -0,0 +1,13 @@
+const message: string = "Hello world";
+console.log(message);
+
+const sum = (a: number, b:number) => {
+    return a + b
+}
+
+console.log('>>> sum = ', sum(2, 2))
+
+
+const test: string = 'phohoccode'
+console.log('>>> length', test.length)
+console.log(">>> UPC", test.toUpperCase());
